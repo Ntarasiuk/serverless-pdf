da3b786878ce569e0a163701514933987de33c21
Nathan Tarasiuk <ntarasiuk@gmail.com>
2019-06-18 17:36:55 -0700

    added documentation link

--- chromium.js
+++ chromium.js
@@ -12,6 +12,9 @@ async function getPdf(type, name, req) {
   let html = req.body.html;
 
   await page.setContent(html, { waitUntil: "networkidle0" });
+  /*
+    PDF options can be found at https://pptr.dev/#?product=Puppeteer&version=v1.17.0&show=api-pagepdfoptions
+  */
   const file = await page.pdf({
     path: `/tmp/${name}.pdf`,
     format: type,
